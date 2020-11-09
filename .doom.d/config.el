c84bdcc8cba418689a04b584a961d2d40c0ecd6a
Richard Jonker <richardjonker2000@gmail.com>
2020-11-09 22:13:04 +0000

    added colors to terminal, powerline and added a bit of transparency to doom emacs

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -22,6 +22,10 @@
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 
+;;transparency?
+(set-frame-parameter (selected-frame) 'alpha '(95 . 90))
+
+(add-to-list 'default-frame-alist '(alpha . (95 . 90)))
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
